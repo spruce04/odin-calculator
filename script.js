a2af72c6058b37d1cbce94ea37b577297b8aa03f
Sam Flax <sam.flax2005@gmail.com>
2022-08-07 00:56:00 +1000

    finished first draft

--- script.js
+++ script.js
@@ -15,13 +15,21 @@ const operatorTimes = document.getElementById("opTimes");
 const operatorMinus = document.getElementById("opMinus");
 const operatorEquals = document.getElementById("opEquals");
 const operatorPlus = document.getElementById("opPlus");
+const operatorDivide = document.getElementById("opDivide")
 const calcDisplay = document.getElementById("display");
+const DEL = document.getElementById("delete")
 let display = '';
-let answer;
+let addBool = false;
+let minusBool = false;
+let multiplyBool = false;
+let divideBool = false;
+let firstNum;
+let secondNum;
+let result;
 
 //basic operator functions
 function add(a, b) {
-    return a + b;
+    return parseInt(a) + parseInt(b);
 }
 
 function subtract(a, b) {
@@ -40,13 +48,80 @@ function updateDisplay(char) {
     display = display + char;
     calcDisplay.textContent = display;
 }
+//Delete Function
+function backspace() {
+    if (display.length == 0)  {
+        return;
+    }
+    else {
+        let displayNew = display.slice(0, -1);
+        display = displayNew;
+        calcDisplay.textContent = display;
+        if (display.indexOf('/') && display.indexOf('+') && display.indexOf('*') && display.indexOf('-')) {
+            addBool = false;
+            minusBool = false;
+            multiplyBool = false;
+            divideBool = false;
+        }
+    }
+}
+DEL.addEventListener("click", function(){
+    backspace();
+})
 
 //AC button
 AC.addEventListener("click", function(){
     location.reload();
 })
 
-//Number buttons
+//Equals function
+function equate(operator) {
+    if (addBool || minusBool || divideBool || multiplyBool) {
+        let index = display.indexOf(operator);
+        secondNum = display.substring(index + 1);
+        switch (operator) {
+            case '+':
+                result = add(firstNum, secondNum);
+                break;
+            case '-':
+                result = subtract(firstNum, secondNum);
+                break;
+            case '*':
+                result = multiply(firstNum, secondNum);
+                break;
+            case '/':
+                result = divide(firstNum, secondNum);
+                break;
+        }
+        display = result;
+        calcDisplay.textContent = display;
+        display = result;
+        addBool = false;
+        minusBool = false;
+        multiplyBool = false;
+        divideBool = false;
+    }
+    else {
+        return;
+    }
+}
+
+//Make buttons functional
+operatorEquals.addEventListener("click", function(){
+    if (addBool) {
+        equate('+');
+    }
+    else if (minusBool) {
+        equate('-');
+    }
+    else if (divideBool) {
+        equate('/');
+    }
+    else {
+        equate('*');
+    }
+    
+})
 numberOne.addEventListener("click", function(){
     updateDisplay('1');
 })
@@ -76,4 +151,39 @@ numberNine.addEventListener("click", function(){
 })
 numberZero.addEventListener("click", function(){
     updateDisplay('0');
+})
+operatorTimes.addEventListener("click", function(){
+    if (addBool || minusBool || divideBool || multiplyBool) {
+        return;
+    }
+    firstNum = calcDisplay.textContent;
+    updateDisplay('*');
+    multiplyBool = true;
+})
+operatorDivide.addEventListener("click", function(){
+    if (addBool || minusBool || divideBool || multiplyBool) {
+        return;
+    }
+    firstNum = calcDisplay.textContent;
+    updateDisplay('/');
+    divideBool = true;
+})
+operatorPlus.addEventListener("click", function(){
+    if (addBool || minusBool || divideBool || multiplyBool) {
+        return;
+    }
+    firstNum = calcDisplay.textContent;
+    updateDisplay('+');
+    addBool = true;
+})
+operatorMinus.addEventListener("click", function(){
+    if (addBool || minusBool || divideBool || multiplyBool) {
+        return;
+    }
+    firstNum = calcDisplay.textContent;
+    updateDisplay('-');
+    minusBool = true;
+})
+floatingPoint.addEventListener("click", function(){
+    updateDisplay('.');
 })
